ba28f1e22dff41551f1fa3126d26ecb69f1ea048
刘新金 <liuxj@cgt.com.cn>
2022-12-08 15:31:16 +0800

    feat: 路由演示

--- my-weibo-code/src/routes/index.js
+++ my-weibo-code/src/routes/index.js
@@ -6,13 +6,27 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params;
+  ctx.body = {
+    title: 'this is profile pages',
+    userName
+  }
+});
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params; // get params from urls
+  ctx.body = {
+    title: 'this is loadMore API',
+    userName,
+    pageIndex
   }
 })
 
