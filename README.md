6ccb86d072bd4fcce6cc60ea79f52c5b9cfea522
刘新金 <liuxj@cgt.com.cn>
2022-12-08 14:58:49 +0800

    refactor: 调整目录结构

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # koa2_weibo_code
-用koa2做一个微博项目
+学习用koa2做一个微博项目 
--- my-weibo-code/src/app.js
+++ my-weibo-code/src/app.js
@@ -6,39 +6,41 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
+// routes 
 const index = require('./routes/index')
 const users = require('./routes/users')
 
 // error handler
-onerror(app)
+onerror(app) // 错误处理 打印到页面
 
-// middlewares
+// middlewares 
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+})) // 解析数据
+app.use(json()) // 解析json
+app.use(logger()) // 日志
+app.use(require('koa-static')(__dirname + '/public')) // 静态资源
 
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
-}))
+})) // 视图  ejs
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   // 记录日志 演示 手写的一个
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
-// routes
+// routes 
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
-});
+}); // 错误处理
 
 module.exports = app
