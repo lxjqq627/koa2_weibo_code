6a61fd79a6fa5f098ab03b943f5329ef67feee05
刘新金 <liuxj@cgt.com.cn>
2022-12-08 18:09:50 +0800

    feat: ejs 演示

--- my-weibo-code/src/routes/index.js
+++ my-weibo-code/src/routes/index.js
@@ -1,24 +1,42 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
-
+    title: 'Hello Koa 2!',
+    isMe: false,
+    blogList: [
+      {
+        id: 1,
+        title: '标题A',
+      },
+      {
+        id: 2,
+        title: '标题B',
+      },
+      {
+        id: 3,
+        title: '标题C',
+      },
+      {
+        id: 4,
+        title: '标题D',
+      },
+    ],
+  });
+});
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    title: 'koa2 json',
+  };
+});
 
 router.get('/profile/:userName', async (ctx, next) => {
   const { userName } = ctx.params;
   ctx.body = {
     title: 'this is profile pages',
-    userName
-  }
+    userName,
+  };
 });
 
 router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
@@ -26,8 +44,8 @@ router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
   ctx.body = {
     title: 'this is loadMore API',
     userName,
-    pageIndex
-  }
-})
+    pageIndex,
+  };
+});
 
-module.exports = router
+module.exports = router;
