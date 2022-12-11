1c63fa4365dbf1ffa3bbca67d6b3ef31f1d61d0c
刘新金 <liuxj@cgt.com.cn>
2022-12-11 17:56:01 +0800

    feat: 抽离登录验证的中间件

---
+++ my-weibo-code/src/middlewares/loginChecks.js
@@ -0,0 +1,42 @@
+/**
+ * @description 登录验证的中间件
+ * @author 刘新金
+ */
+const { ErrorModel } = require('../model/ResModel');
+const { loginCheckFailInfo } = require('../model/ErrorInfo');
+
+/**
+ * API 登录验证
+ * @param {Object} ctx
+ * @param {Function} next
+ */
+async function loginCheck(ctx, next) {
+  if (ctx.session && ctx.session.userInfo) {
+    // 已登录
+    await next();
+    return;
+  }
+  // 未登录
+  ctx.body = new ErrorModel(loginCheckFailInfo);
+}
+
+/**
+ * 页面登录验证
+ * @param {Object} ctx
+ * @param {Function} next
+ */
+async function loginRedirect(ctx, next) {
+  if (ctx.session && ctx.session.userInfo) {
+    // 已登录
+    await next();
+    return;
+  }
+  // 未登录
+  const curUrl = ctx.url;
+  ctx.redirect(`/login?url=${encodeURIComponent(curUrl)}`);
+}
+
+module.exports = {
+  loginCheck,
+  loginRedirect,
+};
